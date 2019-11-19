0459143d6b120298011047033038e872031c58fb
Kognise <felix.mattick@gmail.com>
2019-11-19 16:02:37 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:10
+ENV NODE_ENV production
+
+COPY package.json ./
+COPY yarn.lock ./
+RUN yarn
+
+COPY . .
+RUN yarn build
+
+EXPOSE 3000
+CMD [ "yarn", "start" ]
---
+++ index.js
@@ -0,0 +1,25 @@
+const finalhandler = require('finalhandler')
+const http = require('http')
+const serveIndex = require('serve-index')
+const serveStatic = require('serve-static')
+
+const public = serveStatic('public')
+const serve = serveStatic('files', { index: false })
+const index = serveIndex('files', {
+  stylesheet: './style.css',
+  template: './index.html'
+})
+
+const server = http.createServer((req, res) => {
+  const done = finalhandler(req, res)
+  
+  public(req, res, (err) => {
+    if (err) return done(err)
+    serve(req, res, (err) => {
+      if (err) return done(err)
+      index(req, res, done)
+    })
+  })
+})
+
+server.listen(3000)
